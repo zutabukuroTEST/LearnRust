227069aa590a38b104fc0e77c369613de67a7f8c
zutabukuroTEST <miyamotoak1011@gmail.com>
2022-06-11 14:32:49 +0000

    お試し用。。

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
 fn main() {
-    //mutable
+    // お試し中
+    minus_one_calc(10);
+    let number = arithmetic_progression_calc(10);
+    println!("arithmetic progression result: {}", number);
+    let number = gaus_calc(10);
+    println!("gaus calc result: {}", number);
+
+    // 以下学習中
+
+    // mutable
     let mut s = String::from("hello world");
 
-    //immutable参照(ここから不変なので可変にはできない)
+    // immutable参照(ここから不変なので可変にはできない)
     let word = first_word(&s);
 
-    //OK
+    // OK
     println!("{}", s);
 
-    //OK
+    // OK
     println!("{}", word);
 
-    //NG(5行目で不変借用しており、18行目で使われているから可変借用できない)
-    //s.clear();
+    // NG(5行目で不変借用しており、18行目で使われているから可変借用できない)
+    // s.clear();
 
-    //OK
+    // OK
     println!("the first word is: {}", word);
 
-    //OK
+    // OK
     s.clear();
 }
 
@@ -31,4 +40,25 @@ fn first_word(s: &String) -> &str {
     }
 
     &s[..]
+}
+
+//減算再帰
+fn minus_one_calc(num: u32) -> u32 {
+    println!("minus one number: {}", num);
+    let num = num - 1;
+    if num == 0 { num } else { minus_one_calc(num) }
+}
+
+//等差数列再帰
+fn arithmetic_progression_calc(num: u64) -> u64 {
+    match num {
+        0 => 0,
+        1 => 1,
+        _ => num + arithmetic_progression_calc(num - 1)
+    }
+}
+
+// ガウス計算
+fn gaus_calc(num: u32) -> u32 {
+    ((1 + num)*num)/2
 }
